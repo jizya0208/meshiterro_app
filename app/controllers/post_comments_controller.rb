c7183b012c572d627be87346431c873e84fe788d
EC2 Default User <ec2-user@ip-172-31-54-177.ec2.internal>
2021-05-12 05:16:47 +0000

    [ADD] いいね機能追加

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,16 +1,15 @@
 class PostCommentsController < ApplicationController
   
   def create
-    post_image = PostImage.find(params[:post_image_id])
-    comment = current_user.post_comments.new(post_comment_params) #PostImageモデルのidからレコードを取得。コメントフォームは詳細画面に作成している。
+    post_image = PostImage.find(params[:post_image_id]) #フォームから受け取ったインスタンス変数@post_imageにより投稿を特定する
+    comment = current_user.post_comments.new(post_comment_params)  #PostCommentモデルを基にオブジェクトを生成。user_idはcurrent_user。newの引数でフォームの入力値(commentの内容)を受け取る。
     comment.post_image_id = post_image.id
     comment.save
     redirect_to post_image_path(post_image) 
   end
 
-  
   def destroy
-    PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy
+    PostComment.find_by(id: params[:id], post_image_id: params[:post_image_id]).destroy #params[:id]でPostCommentのidを特定, params[post_image_id]で投稿idを特定し、結びつける。
     redirect_to post_image_path(params[:post_image_id])
   end
   
