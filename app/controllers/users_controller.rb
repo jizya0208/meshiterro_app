bf3056e29dfaa56e45a06386ff484aa2bc35319a
EC2 Default User <ec2-user@ip-172-31-54-177.ec2.internal>
2021-05-12 06:47:07 +0000

    [ADD] ユーザ編集画面

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,8 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     @post_images = @user.post_images.page(params[:id]).reverse_order #ユーザ（@user）に関連付けられた投稿（.post_images）のみ取得
   end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
 end
