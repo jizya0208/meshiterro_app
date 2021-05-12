153a0f2287737fac30e62c3968a4dcc0537cf35f
EC2 Default User <ec2-user@ip-172-31-54-177.ec2.internal>
2021-05-12 06:20:13 +0000

    [ADD] マイページの追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,6 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images.page(params[:id]).reverse_order #ユーザ（@user）に関連付けられた投稿（.post_images）のみ取得
+  end
+end
